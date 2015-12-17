1961cff414733085a39c155feb8f2d773a6129cc
Joel Darling <joel.darling@gmail.com>
2015-12-16 22:27:04 -0500

    added more JSON code

--- weather.js
+++ weather.js
@@ -1,3 +1,20 @@
 var API_KEY = "3aa07c78922b4e4f557f27f03fd0d08d";
 
 var http = require('http');
+
+var request = http.get('http://api.openweathermap.org/data/2.5/forecast?q=10032,us&APPID='+API_KEY,function(response){
+
+  var body = "";
+
+  response.on('data',function(chunk){
+
+    body+=chunk;
+  });
+
+  response.on('end',function(){
+
+    var weather = JSON.parse(response);
+
+    console.log(weather);
+  })
+});
